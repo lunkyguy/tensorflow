c68e0b807ca923ee0bed6e4f235b70d856872437
lunkyguy <zzl19911220@163.com>
2019-01-11 14:09:31 +0800

    学习加练习
    Signed-off-by: lunkyguy <zzl19911220@163.com>

---
+++ lesson2/cifar10_download.py
@@ -0,0 +1,4 @@
+#引入当前目录已经编写好的cifar10模块
+#import cifar10
+#引入tensorflow
+import tensorflow as tf
---
+++ practice/basic_classification.py
@@ -0,0 +1,27 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
